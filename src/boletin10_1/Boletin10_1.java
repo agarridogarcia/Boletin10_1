4e11aa51ed78289bf4a573bebd5f651d94e664c9
Ara <Ara@192.168.0.13>
2016-11-15 21:16:12 +0100

    xogo adivinar número

---
+++ src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,27 @@
+
+package boletin10_1;
+
+
+
+
+
+
+public class Boletin10_1 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        Xogo xogo1=new Xogo ();
+        xogo1.ponNumero();
+        xogo1.adivinaNum();
+        
+        
+   
+
+   
+ }
+    }
+    
+
+
